ac2fd37ed65c8209364f6abb6af422c271b88caa
Scorpion564 <ds1008076@gmail.com>
2022-11-27 20:00:49 +0530

    added all required route in userRoute

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.signUp = async(req,res,next)=>{
+    return res.status(200).send({message:"Hitting the signUp route"});
+}
+
+exports.logIn = async(req,res)=>{
+    return res.status(200).send({message:"hitting LogIn route"});
+}
+
+exports.updateUser = async(req,res)=>{
+    return res.status(200).send({message:"hitting the updateUser route"});
+}
+
+exports.deleteUser = async(req,res)=>{
+    return res.status(200).send({message:"hitting deleteUser route"});
+}
+
+exports.data = async(req,res)=>{
+    return res.status(200).send({message:"hitting the data route"});
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
+const userController = require("../controllers/userController");
 
-router.post("/",(req,res)=>{
-    res.send("API is working");
-})
+router.post("/",userController.signUp);
+router.post("/login",userController.logIn);
+router.patch('/:userId',userController.updateUser);
+router.delete('/:userId',userController.deleteUser);
+router.get('/data',userController.data);
 
 module.exports=router;
