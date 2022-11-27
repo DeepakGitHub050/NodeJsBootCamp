fe9ca29adae77898881bd02e3ccc53f435960285
Scorpion564 <ds1008076@gmail.com>
2022-11-27 22:43:27 +0530

    testing

--- database/db.js
+++ database/db.js
@@ -4,15 +4,15 @@ require('dotenv').config();
 const database = mongoose.connect(
     process.env.DB_URL,
     {
-        useNewUrlParser:true,
-        useUnifieldTopology:true,
-        useFindAndModify:false,
-        useCreateIndex:true
+        useNewUrlParser: true,
+        useUnifieldTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
     },
-    (error)=>{
-        if(!error)
+    (error) => {
+        if (!error)
             console.log("connected to MongoDB");
         else
-            console.log("Failed to connect to MongoDB\n"+error)
+            console.log("Failed to connect to MongoDB\n" + error);
     }
 )
